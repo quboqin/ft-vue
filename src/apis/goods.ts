76a0e1f4d67dabd0a72c1534244eed3c9d230f3b
Qubo Qin <quboqin0710@gmail.com>
2021-09-27 13:45:14 +0800

    remove mock data

--- src/apis/goods.ts
+++ src/apis/goods.ts
@@ -2,8 +2,9 @@ import { result } from '@/utils/axios'
 
 import { Good } from 'quboqin-lib/lib/goods'
 
-import goods from '@/mock/goods.json'
+// import goods from '@/mock/goods.json'
 
 export function getAllGoods(): Promise<Good[] | void> {
-  return result('get', '/goods', null, goods as Good[])
+  // return result('get', '/goods', null, goods as Good[])
+  return result('get', '/goods', null)
 }
